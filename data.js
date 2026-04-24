9a47b65b6221c4bc069528070374015222350003
Davidhzk2 <guffyherrera150@gmail.com>
2026-04-23 21:45:45 -0500

    Base files

---
+++ data.js
@@ -0,0 +1,317 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_20260423_120912_00_148-entrada",
+      "name": "IMG_20260423_120912_00_148-Entrada",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.9186883733528362,
+        "pitch": 0.013127931198946285,
+        "fov": 1.4108347438219848
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.038137698641048,
+          "pitch": 0.07469024083012954,
+          "rotation": 1.5707963267948966,
+          "target": "1-img_20260423_120530_00_141-sala1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-img_20260423_120530_00_141-sala1",
+      "name": "IMG_20260423_120530_00_141-Sala1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.4108347438219848
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3255209831175758,
+          "pitch": 0.13591435087676373,
+          "rotation": 3.9269908169872414,
+          "target": "2-img_20260423_120555_00_142-sala2"
+        },
+        {
+          "yaw": 2.8557287860095713,
+          "pitch": 0.47142727090991166,
+          "rotation": 1.5707963267948966,
+          "target": "0-img_20260423_120912_00_148-entrada"
+        },
+        {
+          "yaw": -0.14191011664190967,
+          "pitch": 0.17479072342062096,
+          "rotation": 10.995574287564278,
+          "target": "5-img_20260423_120821_00_146-patio"
+        },
+        {
+          "yaw": -0.12183843269095362,
+          "pitch": -0.0009832591288798653,
+          "rotation": 0,
+          "target": "5-img_20260423_120821_00_146-patio"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-img_20260423_120555_00_142-sala2",
+      "name": "IMG_20260423_120555_00_142-Sala2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5270433849613898,
+          "pitch": 0.061317145993740496,
+          "rotation": 0,
+          "target": "1-img_20260423_120530_00_141-sala1"
+        },
+        {
+          "yaw": 0.560754562270418,
+          "pitch": 0.1063404357559925,
+          "rotation": 0,
+          "target": "3-img_20260423_120658_00_144-sala-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-img_20260423_120658_00_144-sala-3",
+      "name": "IMG_20260423_120658_00_144-Sala-3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.997085794677024,
+          "pitch": 0.24809062235752144,
+          "rotation": 0,
+          "target": "4-img_20260423_120723_00_145-hall1"
+        },
+        {
+          "yaw": 2.998441136901044,
+          "pitch": 0.03774217992252815,
+          "rotation": 1.5707963267948966,
+          "target": "5-img_20260423_120821_00_146-patio"
+        },
+        {
+          "yaw": -0.16512006327874218,
+          "pitch": 0.13451770760449655,
+          "rotation": 0,
+          "target": "0-img_20260423_120912_00_148-entrada"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-img_20260423_120723_00_145-hall1",
+      "name": "IMG_20260423_120723_00_145-Hall1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4252188960932148,
+          "pitch": 0.2718674801053833,
+          "rotation": 1.5707963267948966,
+          "target": "3-img_20260423_120658_00_144-sala-3"
+        },
+        {
+          "yaw": 1.4182946710607425,
+          "pitch": 0.05560603053802993,
+          "rotation": 7.853981633974483,
+          "target": "0-img_20260423_120912_00_148-entrada"
+        },
+        {
+          "yaw": -1.6866048885606535,
+          "pitch": 0.1358937025012068,
+          "rotation": 1.5707963267948966,
+          "target": "5-img_20260423_120821_00_146-patio"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-img_20260423_120821_00_146-patio",
+      "name": "IMG_20260423_120821_00_146-patio",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5147855400370105,
+          "pitch": 0.26115976223258386,
+          "rotation": 0,
+          "target": "4-img_20260423_120723_00_145-hall1"
+        },
+        {
+          "yaw": -1.5188178389599454,
+          "pitch": 0.08307225032171672,
+          "rotation": 0,
+          "target": "3-img_20260423_120658_00_144-sala-3"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Demo_Casa_1",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
